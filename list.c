21b5afb14b756146e75b2979b5366ef9e0283c85
M4cr0v <M4cr0v@gmail.com>
2012-10-02 10:45:35 +0800

    2012-10-02 10:45 add cursor method of list

--- list.c
+++ list.c
@@ -22,14 +22,18 @@ struct Node {
     Position Next;
 } /* optional variable list */;
 
+#ifdef LIST_PTR
 List MakeEmpty(List L)
 {
-    List X=L->Next, Y;
-    while (X != NULL) {
-        Y = X->Next;
-        free(X);
-        X = Y;
+    Position P, Temp;
+    P = L->Next;
+    L->Next = NULL;
+    while (P != NULL) {
+        Temp = P->Next;
+        free(P);
+        P = Temp;
     }
+
     return L;
 }
 
@@ -122,10 +126,20 @@ List CreatList(int size)
     Position Head, P;
     int i, NodeSize;
     NodeSize = sizeof(struct Node);
+
+//    //if you malloc all nodes' space once, then you cannot free a single node later
+//    if ((Head = malloc(NodeSize * (size+1)))==NULL) {
+//        error_at_line(0, errno, __FILE__, __LINE__, "%s", "malloc");
+//    }
+//    P = Head;
+//    for (i = 0; i < size; i++) {
+//        P->Next = P+1;
+//        P = P->Next;                         /* or you can use ++P; */
+//    }
+    
     if ((Head = malloc(NodeSize))==NULL) {
         error_at_line(0, errno, __FILE__, __LINE__, "%s", "malloc");
     }
-    Head->Next = NULL;
     P = Head;
     for (i = 0; i < size; i++) {
         if ((P->Next = malloc(NodeSize))==NULL) {
@@ -133,6 +147,7 @@ List CreatList(int size)
         }
         P = P->Next;
     }
+    P->Next = NULL;
     return Head;
 }
 
@@ -170,7 +185,8 @@ void PrintList(List L)
 
 void ListTest(void)
 {
-    int l=10, i;
+    int l=10, i, temp=3;
+    int *memtest;
     List L, P;
     L = CreatList(l);
     P = L->Next;
@@ -178,9 +194,227 @@ void ListTest(void)
         P->Element = i;
         P = P->Next;
     }
+
     printf("Origin List:\n");
     PrintList(L);
+    
+    P = FindPrevious(temp, L);
+    Insert(temp+2, L, P);
+    printf("Inserted List:\n");
+    PrintList(L);
+
+    Delete(temp, L);
+    printf("Deleted List:\n");
+    PrintList(L);
+    
     L = ReverseList(L);
     printf("Reversed List:\n");
     PrintList(L);
 }
+#endif
+
+
+#ifdef LIST_CURSOR
+struct Node CursorSpace[SpaceSize];
+
+void InitializeCursorSpace(void)
+{
+    int i;
+    for (i = 0; i < SpaceSize; i++) {
+        CursorSpace[i].Next = i+1;
+    }
+    CursorSpace[SpaceSize-1].Next = 0;
+
+}
+
+static Position CursorAlloc(void)
+{
+    Position P;
+    P = CursorSpace[0].Next;
+    CursorSpace[0].Next = CursorSpace[P].Next;
+
+    return P;
+}
+
+static void CursorFree(Position P)
+{
+    CursorSpace[P].Next = CursorSpace[0].Next;
+    CursorSpace[0].Next = P;
+}
+
+List MakeEmpty(List L)
+{
+    Position P;
+    P = L;
+    while (CursorSpace[P].Next != 0) {
+        P = CursorSpace[P].Next;
+    }
+    CursorSpace[P].Next = CursorSpace[0].Next;
+    CursorSpace[0].Next = CursorSpace[L].Next;
+    CursorSpace[L].Next = 0;
+    return L;
+}
+
+int IsEmpty(const List L)
+{
+    return CursorSpace[L].Next == 0;
+}
+
+int IsLast(const Position P, const List L)
+{
+    return CursorSpace[P].Next == 0;
+}
+
+Position Find(ElementType X, const List L)
+{
+    Position P;
+    P = CursorSpace[L].Next;
+    while (P && CursorSpace[P].Next != X) {
+        P = CursorSpace[P].Next;
+    }
+    return P;
+}
+
+void Delete(ElementType X, const List L)
+{
+    Position P, TempCell;
+    P = FindPrevious(X, L);
+    if (!IsLast(P, L)) {
+        TempCell = CursorSpace[P].Next;
+        CursorSpace[P].Next = CursorSpace[TempCell].Next;
+        CursorFree(TempCell);
+    }
+}
+
+Position FindPrevious(ElementType X, const List L)
+{
+    Position P;
+    P = L;
+    while (CursorSpace[P].Next && CursorSpace[CursorSpace[P].Next].Element != X) {
+        P = CursorSpace[P].Next;
+    }
+    return P;
+}
+
+void Insert(ElementType X, List L, Position P)
+{
+    Position TempCell;
+    TempCell = CursorAlloc();
+    if (TempCell == 0) {
+        error_at_line(0, ENOMEM, __FILE__, __LINE__, "%s", "CursorAlloc");
+    }
+    CursorSpace[TempCell].Element = X;
+    CursorSpace[TempCell].Next = CursorSpace[P].Next;
+    CursorSpace[P].Next = TempCell;
+}
+
+void DeleteList(List L)
+{
+    Position P;
+    P = L;
+    while (CursorSpace[P].Next != 0) {
+        P = CursorSpace[P].Next;
+    }
+    CursorSpace[P].Next = CursorSpace[0].Next;
+    CursorSpace[0].Next = CursorSpace[L].Next;
+    CursorSpace[L].Next = 0;
+}
+
+Position Header(const List L)
+{
+    return L;
+}
+
+Position First(const List L)
+{
+    return CursorSpace[L].Next;
+}
+
+Position Advance(const Position P)
+{
+    return CursorSpace[P].Next;
+}
+
+ElementType Retrieve(const Position P)
+{
+    return CursorSpace[P].Element;
+}
+
+List CreatList(int size)
+{
+    Position Head, P=0;
+    int i;
+    for (i = 0; i <= size; i++) {
+        P = CursorSpace[P].Next;
+        if (P == 0) {
+            error_at_line(0, ENOMEM, __FILE__, __LINE__, "%s", "CursorAlloc");
+        }
+    }
+    Head = CursorSpace[0].Next;
+    CursorSpace[0].Next = CursorSpace[P].Next;
+    CursorSpace[P].Next = 0;
+    return Head;
+}
+
+List ReverseList(List L)
+{
+    Position P1, P2, P3;
+    if (L==0 || CursorSpace[L].Next==0) {
+        return L;
+    }
+    P1 = CursorSpace[L].Next;
+    P2 = CursorSpace[P1].Next;
+    P3 = CursorSpace[P2].Next;
+    CursorSpace[P1].Next = 0;
+    while (P3!=0) {
+        CursorSpace[P2].Next = P1;
+        P1 = P2;
+        P2 = P3;
+        P3 = CursorSpace[P3].Next;
+    }
+    CursorSpace[P2].Next = P1;
+    CursorSpace[L].Next = P2;
+    return L;
+
+}
+
+void PrintList(List L)
+{
+    printf("Head->");
+    while (CursorSpace[L].Next!=0) {
+//        printf("%d(%d)->", CursorSpace[L].Next, CursorSpace[CursorSpace[L].Next].Element);
+        printf("%d->", CursorSpace[CursorSpace[L].Next].Element);
+        L = CursorSpace[L].Next;
+    }
+    printf("NULL\n");
+}
+
+void ListTest(void)
+{
+    int l=10, i, temp = 3;
+    List L, P;
+    InitializeCursorSpace();
+    L = CreatList(l);
+    P = CursorSpace[L].Next;
+    for (i = 0; i < l; i++) {
+        CursorSpace[P].Element = i;
+        P = CursorSpace[P].Next;
+    }
+    printf("Origin List:\n");
+    PrintList(L);
+
+    P = FindPrevious(temp, L);
+    Insert(temp+2, L, P);
+    printf("Inserted List:\n");
+    PrintList(L);
+
+    Delete(temp, L);
+    printf("Deleted List:\n");
+    PrintList(L);
+
+    L = ReverseList(L);
+    printf("Reversed List:\n");
+    PrintList(L);
+}
+
+#endif
--- list.h
+++ list.h
@@ -23,28 +23,41 @@ extern "C" {
 #endif
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <error.h>
 #include <errno.h>
-typedef int ElementType;
-extern struct Node;
+#define LIST_PTR
+//#define LIST_CURSOR
+
+
+
+struct Node;
+#ifdef LIST_PTR
 typedef struct Node *PtrToNode;
+#endif
+
+#ifdef LIST_CURSOR
+#define SpaceSize 100
+typedef int PtrToNode;
+void InitializeCursorSpace(void);
+#endif
+
+typedef int ElementType;
 typedef PtrToNode List;
 typedef PtrToNode Position;
 List MakeEmpty(List L);
-int IsEmpty(List L);
-int IsLast(Position P, List L);
-Position Find(ElementType X, List L);
-Position FindPrevious(ElementType X, List L);
+int IsEmpty(const List L);
+int IsLast(const Position P, const List L);
+Position Find(ElementType X, const List L);
 void Delete(ElementType X, List L);
+Position FindPrevious(ElementType X, const List L);
 void Insert(ElementType X, List L, Position P);
 void DeleteList(List L);
 Position Header(List L);
 Position First(List L);
-Position Advance(List L);
-ElementType Retrieve(Position P);
+Position Advance(const Position P);
+ElementType Retrieve(const Position P);
 void PrintList(List L);
-
-
 
 
 #ifdef __cplusplus
